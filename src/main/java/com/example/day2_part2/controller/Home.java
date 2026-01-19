6afe33724e95be04160b8c3ff91dce89b4585007
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-01-19 15:12:59 +0530

    created 5 student data

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -3,12 +3,18 @@ package com.example.day2_part2.controller;
 import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.*;
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent() {
-        StudentModel student = new StudentModel(1,"Mahesh","maheshgoyal20032004@gmail.com");
+    public List<StudentModel> getStudent() {
+
+        ArrayList<StudentModel> student = new ArrayList<>();
+        student.add(new StudentModel(1, "Mahesh", "mahesh@gmail.com"));
+        student.add(new StudentModel(2, "Rahul", "rahul@gmail.com"));
+        student.add(new StudentModel(3, "Amit", "amit@gmail.com"));
+        student.add(new StudentModel(4, "Sneha", "sneha@gmail.com"));
+        student.add(new StudentModel(5, "Priya", "priya@gmail.com"));
         return student;
     }
 
