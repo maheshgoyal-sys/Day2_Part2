0fb18f9abf27509ada384d5ec3db3dc6d2a6cc19
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-01-19 15:04:09 +0530

    Added our first api

--- src/main/java/com/example/day2_part2/controller/Home.java
+++ src/main/java/com/example/day2_part2/controller/Home.java
@@ -1,4 +1,15 @@
 package com.example.day2_part2.controller;
 
+import com.example.day2_part2.model.StudentModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Home {
+    @GetMapping("/")
+    public StudentModel getStudent() {
+        StudentModel student = new StudentModel(1,"Mahesh","maheshgoyal20032004@gmail.com");
+        return student;
+    }
+
 }
--- src/main/java/com/example/day2_part2/model/StudentModel.java
+++ src/main/java/com/example/day2_part2/model/StudentModel.java
@@ -1,4 +1,38 @@
 package com.example.day2_part2.model;
 
 public class StudentModel {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+    public StudentModel(int id,String name,String email){
+        this.id = id;
+        this.name = name;
+        this.email= email;
+    }
+
 }
